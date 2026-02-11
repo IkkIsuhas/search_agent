757334d87fd38f6415725016ae69336b0953828d
IkkIsuhas <suhasm004@gmail.com>
2026-02-11 08:39:45 +0530

    create basic agent

--- agent.py
+++ agent.py
@@ -99,32 +99,6 @@ def text_parser(text:str)->str:
 
     return f"Characturs: {char_count} Words: {word_count}"
 
-prompt_template = """Answer the following questions as best you can. You have access to the following tools:
-
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin!
-
-Question: {input}
-Thought: {agent_scratchpad}
-"""
-
-prompt = PromptTemplate(
-     template=prompt_template,
-     input_variables=["input", "agent_scratchpad"] 
-)
-
 llm = ChatOpenAI(
      api_key=os.getenv("OPENROUTER_API_KEY"),
     base_url="https://openrouter.ai/api/v1",
